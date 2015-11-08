4ded6a119a73d6dd0b9996c700a9dea60d5df635
Chris Brown <christopher.r.brown@aggiemail.usu.edu>
2015-11-08 00:59:25 -0700

    Lobby visuals created, and lobby finder visuals created.

---
+++ SET/Lobby.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace SET
+{
+    partial class Lobby
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.quitLobbyButton = new System.Windows.Forms.Button();
+            this.startGameButton = new System.Windows.Forms.Button();
+            this.optionsGroupBox = new System.Windows.Forms.GroupBox();
+            this.playerList = new System.Windows.Forms.GroupBox();
+            this.player1Label = new System.Windows.Forms.Label();
+            this.player2Label = new System.Windows.Forms.Label();
+            this.player3Label = new System.Windows.Forms.Label();
+            this.player4Label = new System.Windows.Forms.Label();
+            this.player5Label = new System.Windows.Forms.Label();
+            this.deckOptions = new System.Windows.Forms.GroupBox();
+            this.deckOptionsCheckBox1 = new System.Windows.Forms.CheckBox();
+            this.deckOptionsCheckBox2 = new System.Windows.Forms.CheckBox();
+            this.deckOptionsCheckBox3 = new System.Windows.Forms.CheckBox();
+            this.deckOptionsCheckBox4 = new System.Windows.Forms.CheckBox();
+            this.beginnerModeRadioButton = new System.Windows.Forms.RadioButton();
+            this.tutorialModeRadioButton = new System.Windows.Forms.RadioButton();
+            this.numberOfSetsTextBot = new System.Windows.Forms.TextBox();
+            this.numberOfSetsLabel = new System.Windows.Forms.Label();
+            this.normalModeRadioButton = new System.Windows.Forms.RadioButton();
+            this.optionsGroupBox.SuspendLayout();
+            this.playerList.SuspendLayout();
+            this.deckOptions.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // quitLobbyButton
+            // 
+            this.quitLobbyButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.quitLobbyButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.quitLobbyButton.Location = new System.Drawing.Point(611, 440);
+            this.quitLobbyButton.Name = "quitLobbyButton";
+            this.quitLobbyButton.Size = new System.Drawing.Size(111, 44);
+            this.quitLobbyButton.TabIndex = 4;
+            this.quitLobbyButton.Text = "Quit";
+            this.quitLobbyButton.UseVisualStyleBackColor = true;
+            // 
+            // startGameButton
+            // 
+            this.startGameButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.startGameButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startGameButton.Location = new System.Drawing.Point(145, 502);
+            this.startGameButton.Name = "startGameButton";
+            this.startGameButton.Size = new System.Drawing.Size(111, 44);
+            this.startGameButton.TabIndex = 5;
+            this.startGameButton.Text = "Start";
+            this.startGameButton.UseVisualStyleBackColor = true;
+            // 
+            // optionsGroupBox
+            // 
+            this.optionsGroupBox.Controls.Add(this.normalModeRadioButton);
+            this.optionsGroupBox.Controls.Add(this.numberOfSetsLabel);
+            this.optionsGroupBox.Controls.Add(this.numberOfSetsTextBot);
+            this.optionsGroupBox.Controls.Add(this.tutorialModeRadioButton);
+            this.optionsGroupBox.Controls.Add(this.beginnerModeRadioButton);
+            this.optionsGroupBox.Controls.Add(this.deckOptions);
+            this.optionsGroupBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.optionsGroupBox.Location = new System.Drawing.Point(71, 46);
+            this.optionsGroupBox.Name = "optionsGroupBox";
+            this.optionsGroupBox.Size = new System.Drawing.Size(266, 438);
+            this.optionsGroupBox.TabIndex = 6;
+            this.optionsGroupBox.TabStop = false;
+            this.optionsGroupBox.Text = "Options";
+            // 
+            // playerList
+            // 
+            this.playerList.Controls.Add(this.player5Label);
+            this.playerList.Controls.Add(this.player4Label);
+            this.playerList.Controls.Add(this.player3Label);
+            this.playerList.Controls.Add(this.player2Label);
+            this.playerList.Controls.Add(this.player1Label);
+            this.playerList.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playerList.Location = new System.Drawing.Point(561, 97);
+            this.playerList.Name = "playerList";
+            this.playerList.Size = new System.Drawing.Size(200, 324);
+            this.playerList.TabIndex = 7;
+            this.playerList.TabStop = false;
+            this.playerList.Text = "Players";
+            // 
+            // player1Label
+            // 
+            this.player1Label.AutoSize = true;
+            this.player1Label.Location = new System.Drawing.Point(6, 30);
+            this.player1Label.Name = "player1Label";
+            this.player1Label.Size = new System.Drawing.Size(102, 29);
+            this.player1Label.TabIndex = 0;
+            this.player1Label.Text = "1 - Chris";
+            // 
+            // player2Label
+            // 
+            this.player2Label.AutoSize = true;
+            this.player2Label.Location = new System.Drawing.Point(6, 90);
+            this.player2Label.Name = "player2Label";
+            this.player2Label.Size = new System.Drawing.Size(102, 29);
+            this.player2Label.TabIndex = 1;
+            this.player2Label.Text = "1 - Chris";
+            // 
+            // player3Label
+            // 
+            this.player3Label.AutoSize = true;
+            this.player3Label.Location = new System.Drawing.Point(6, 150);
+            this.player3Label.Name = "player3Label";
+            this.player3Label.Size = new System.Drawing.Size(102, 29);
+            this.player3Label.TabIndex = 2;
+            this.player3Label.Text = "1 - Chris";
+            // 
+            // player4Label
+            // 
+            this.player4Label.AutoSize = true;
+            this.player4Label.Location = new System.Drawing.Point(6, 210);
+            this.player4Label.Name = "player4Label";
+            this.player4Label.Size = new System.Drawing.Size(102, 29);
+            this.player4Label.TabIndex = 3;
+            this.player4Label.Text = "1 - Chris";
+            // 
+            // player5Label
+            // 
+            this.player5Label.AutoSize = true;
+            this.player5Label.Location = new System.Drawing.Point(6, 270);
+            this.player5Label.Name = "player5Label";
+            this.player5Label.Size = new System.Drawing.Size(102, 29);
+            this.player5Label.TabIndex = 4;
+            this.player5Label.Text = "1 - Chris";
+            // 
+            // deckOptions
+            // 
+            this.deckOptions.Controls.Add(this.deckOptionsCheckBox4);
+            this.deckOptions.Controls.Add(this.deckOptionsCheckBox3);
+            this.deckOptions.Controls.Add(this.deckOptionsCheckBox2);
+            this.deckOptions.Controls.Add(this.deckOptionsCheckBox1);
+            this.deckOptions.Location = new System.Drawing.Point(26, 36);
+            this.deckOptions.Name = "deckOptions";
+            this.deckOptions.Size = new System.Drawing.Size(216, 194);
+            this.deckOptions.TabIndex = 0;
+            this.deckOptions.TabStop = false;
+            this.deckOptions.Text = "Deck Options";
+            // 
+            // deckOptionsCheckBox1
+            // 
+            this.deckOptionsCheckBox1.AutoSize = true;
+            this.deckOptionsCheckBox1.Location = new System.Drawing.Point(15, 33);
+            this.deckOptionsCheckBox1.Name = "deckOptionsCheckBox1";
+            this.deckOptionsCheckBox1.Size = new System.Drawing.Size(169, 33);
+            this.deckOptionsCheckBox1.TabIndex = 0;
+            this.deckOptionsCheckBox1.Text = "Single Color";
+            this.deckOptionsCheckBox1.UseVisualStyleBackColor = true;
+            // 
+            // deckOptionsCheckBox2
+            // 
+            this.deckOptionsCheckBox2.AutoSize = true;
+            this.deckOptionsCheckBox2.Location = new System.Drawing.Point(15, 72);
+            this.deckOptionsCheckBox2.Name = "deckOptionsCheckBox2";
+            this.deckOptionsCheckBox2.Size = new System.Drawing.Size(180, 33);
+            this.deckOptionsCheckBox2.TabIndex = 1;
+            this.deckOptionsCheckBox2.Text = "Single Shade";
+            this.deckOptionsCheckBox2.UseVisualStyleBackColor = true;
+            // 
+            // deckOptionsCheckBox3
+            // 
+            this.deckOptionsCheckBox3.AutoSize = true;
+            this.deckOptionsCheckBox3.Location = new System.Drawing.Point(15, 111);
+            this.deckOptionsCheckBox3.Name = "deckOptionsCheckBox3";
+            this.deckOptionsCheckBox3.Size = new System.Drawing.Size(197, 33);
+            this.deckOptionsCheckBox3.TabIndex = 2;
+            this.deckOptionsCheckBox3.Text = "Single Number";
+            this.deckOptionsCheckBox3.UseVisualStyleBackColor = true;
+            // 
+            // deckOptionsCheckBox4
+            // 
+            this.deckOptionsCheckBox4.AutoSize = true;
+            this.deckOptionsCheckBox4.Location = new System.Drawing.Point(15, 150);
+            this.deckOptionsCheckBox4.Name = "deckOptionsCheckBox4";
+            this.deckOptionsCheckBox4.Size = new System.Drawing.Size(180, 33);
+            this.deckOptionsCheckBox4.TabIndex = 3;
+            this.deckOptionsCheckBox4.Text = "Single Shape";
+            this.deckOptionsCheckBox4.UseVisualStyleBackColor = true;
+            // 
+            // beginnerModeRadioButton
+            // 
+            this.beginnerModeRadioButton.AutoSize = true;
+            this.beginnerModeRadioButton.Location = new System.Drawing.Point(26, 252);
+            this.beginnerModeRadioButton.Name = "beginnerModeRadioButton";
+            this.beginnerModeRadioButton.Size = new System.Drawing.Size(200, 33);
+            this.beginnerModeRadioButton.TabIndex = 1;
+            this.beginnerModeRadioButton.Text = "Beginner Mode";
+            this.beginnerModeRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // tutorialModeRadioButton
+            // 
+            this.tutorialModeRadioButton.AutoSize = true;
+            this.tutorialModeRadioButton.Location = new System.Drawing.Point(26, 292);
+            this.tutorialModeRadioButton.Name = "tutorialModeRadioButton";
+            this.tutorialModeRadioButton.Size = new System.Drawing.Size(184, 33);
+            this.tutorialModeRadioButton.TabIndex = 2;
+            this.tutorialModeRadioButton.Text = "Tutorial Mode";
+            this.tutorialModeRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // numberOfSetsTextBot
+            // 
+            this.numberOfSetsTextBot.Location = new System.Drawing.Point(203, 373);
+            this.numberOfSetsTextBot.MaxLength = 2;
+            this.numberOfSetsTextBot.Name = "numberOfSetsTextBot";
+            this.numberOfSetsTextBot.Size = new System.Drawing.Size(35, 34);
+            this.numberOfSetsTextBot.TabIndex = 3;
+            // 
+            // numberOfSetsLabel
+            // 
+            this.numberOfSetsLabel.AutoSize = true;
+            this.numberOfSetsLabel.Location = new System.Drawing.Point(20, 374);
+            this.numberOfSetsLabel.Name = "numberOfSetsLabel";
+            this.numberOfSetsLabel.Size = new System.Drawing.Size(180, 29);
+            this.numberOfSetsLabel.TabIndex = 4;
+            this.numberOfSetsLabel.Text = "Number of Sets";
+            // 
+            // normalModeRadioButton
+            // 
+            this.normalModeRadioButton.AutoSize = true;
+            this.normalModeRadioButton.Checked = true;
+            this.normalModeRadioButton.Location = new System.Drawing.Point(26, 331);
+            this.normalModeRadioButton.Name = "normalModeRadioButton";
+            this.normalModeRadioButton.Size = new System.Drawing.Size(181, 33);
+            this.normalModeRadioButton.TabIndex = 5;
+            this.normalModeRadioButton.TabStop = true;
+            this.normalModeRadioButton.Text = "Normal Mode";
+            this.normalModeRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // Lobby
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(882, 655);
+            this.Controls.Add(this.playerList);
+            this.Controls.Add(this.optionsGroupBox);
+            this.Controls.Add(this.startGameButton);
+            this.Controls.Add(this.quitLobbyButton);
+            this.MaximumSize = new System.Drawing.Size(1920, 1080);
+            this.MinimumSize = new System.Drawing.Size(900, 450);
+            this.Name = "Lobby";
+            this.Text = "Lobby";
+            this.optionsGroupBox.ResumeLayout(false);
+            this.optionsGroupBox.PerformLayout();
+            this.playerList.ResumeLayout(false);
+            this.playerList.PerformLayout();
+            this.deckOptions.ResumeLayout(false);
+            this.deckOptions.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button quitLobbyButton;
+        private System.Windows.Forms.Button startGameButton;
+        private System.Windows.Forms.GroupBox optionsGroupBox;
+        private System.Windows.Forms.GroupBox playerList;
+        private System.Windows.Forms.Label numberOfSetsLabel;
+        private System.Windows.Forms.TextBox numberOfSetsTextBot;
+        private System.Windows.Forms.RadioButton tutorialModeRadioButton;
+        private System.Windows.Forms.RadioButton beginnerModeRadioButton;
+        private System.Windows.Forms.GroupBox deckOptions;
+        private System.Windows.Forms.CheckBox deckOptionsCheckBox4;
+        private System.Windows.Forms.CheckBox deckOptionsCheckBox3;
+        private System.Windows.Forms.CheckBox deckOptionsCheckBox2;
+        private System.Windows.Forms.CheckBox deckOptionsCheckBox1;
+        private System.Windows.Forms.Label player5Label;
+        private System.Windows.Forms.Label player4Label;
+        private System.Windows.Forms.Label player3Label;
+        private System.Windows.Forms.Label player2Label;
+        private System.Windows.Forms.Label player1Label;
+        private System.Windows.Forms.RadioButton normalModeRadioButton;
+    }
+}
---
+++ SET/LobbyFinder.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace SET
+{
+    partial class LobbyFinder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lobbyList = new System.Windows.Forms.ListBox();
+            this.lobbyNameLabel = new System.Windows.Forms.Label();
+            this.playersInLobbyLabel = new System.Windows.Forms.Label();
+            this.joinLobbyButton = new System.Windows.Forms.Button();
+            this.createLobbyButton = new System.Windows.Forms.Button();
+            this.exitLobbyFinderButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lobbyList
+            // 
+            this.lobbyList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lobbyList.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.lobbyList.Font = new System.Drawing.Font("Calibri", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lobbyList.FormattingEnabled = true;
+            this.lobbyList.ItemHeight = 33;
+            this.lobbyList.Items.AddRange(new object[] {
+            "Example Lobby 1                                  3/5",
+            "Example Lobby 2                                  1/5",
+            "Example Lobby 3                                  4/5",
+            "Example Lobby 4                                  2/5",
+            "Example Lobby 5                                  1/5",
+            "Example Lobby 6                                  3/5",
+            "Example Lobby 7                                  4/5",
+            "Example Lobby 8                                  2/5",
+            "Example Lobby 9                                  2/5",
+            "Example Lobby 10                                3/5",
+            "Example Lobby 11                                1/5",
+            "Example Lobby 12                                4/5",
+            "Example Lobby 13                                3/5",
+            "Example Lobby 14                                4/5",
+            "Example Lobby 15                                4/5",
+            "Example Lobby 16                                1/5"});
+            this.lobbyList.Location = new System.Drawing.Point(211, 123);
+            this.lobbyList.Name = "lobbyList";
+            this.lobbyList.Size = new System.Drawing.Size(460, 334);
+            this.lobbyList.TabIndex = 0;
+            // 
+            // lobbyNameLabel
+            // 
+            this.lobbyNameLabel.AutoSize = true;
+            this.lobbyNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lobbyNameLabel.Location = new System.Drawing.Point(205, 88);
+            this.lobbyNameLabel.Name = "lobbyNameLabel";
+            this.lobbyNameLabel.Size = new System.Drawing.Size(185, 32);
+            this.lobbyNameLabel.TabIndex = 1;
+            this.lobbyNameLabel.Text = "Lobby Name";
+            // 
+            // playersInLobbyLabel
+            // 
+            this.playersInLobbyLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.playersInLobbyLabel.AutoSize = true;
+            this.playersInLobbyLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playersInLobbyLabel.Location = new System.Drawing.Point(558, 88);
+            this.playersInLobbyLabel.Name = "playersInLobbyLabel";
+            this.playersInLobbyLabel.Size = new System.Drawing.Size(131, 32);
+            this.playersInLobbyLabel.TabIndex = 2;
+            this.playersInLobbyLabel.Text = "In Lobby";
+            // 
+            // joinLobbyButton
+            // 
+            this.joinLobbyButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.joinLobbyButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.joinLobbyButton.Location = new System.Drawing.Point(211, 464);
+            this.joinLobbyButton.Name = "joinLobbyButton";
+            this.joinLobbyButton.Size = new System.Drawing.Size(111, 44);
+            this.joinLobbyButton.TabIndex = 3;
+            this.joinLobbyButton.Text = "Join";
+            this.joinLobbyButton.UseVisualStyleBackColor = true;
+            // 
+            // createLobbyButton
+            // 
+            this.createLobbyButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.createLobbyButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.createLobbyButton.Location = new System.Drawing.Point(379, 464);
+            this.createLobbyButton.Name = "createLobbyButton";
+            this.createLobbyButton.Size = new System.Drawing.Size(119, 44);
+            this.createLobbyButton.TabIndex = 5;
+            this.createLobbyButton.Text = "Create";
+            this.createLobbyButton.UseVisualStyleBackColor = true;
+            // 
+            // exitLobbyFinderButton
+            // 
+            this.exitLobbyFinderButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.exitLobbyFinderButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitLobbyFinderButton.Location = new System.Drawing.Point(552, 464);
+            this.exitLobbyFinderButton.Name = "exitLobbyFinderButton";
+            this.exitLobbyFinderButton.Size = new System.Drawing.Size(119, 44);
+            this.exitLobbyFinderButton.TabIndex = 6;
+            this.exitLobbyFinderButton.Text = "Exit";
+            this.exitLobbyFinderButton.UseVisualStyleBackColor = true;
+            // 
+            // LobbyFinder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(882, 655);
+            this.Controls.Add(this.exitLobbyFinderButton);
+            this.Controls.Add(this.createLobbyButton);
+            this.Controls.Add(this.joinLobbyButton);
+            this.Controls.Add(this.playersInLobbyLabel);
+            this.Controls.Add(this.lobbyNameLabel);
+            this.Controls.Add(this.lobbyList);
+            this.MaximumSize = new System.Drawing.Size(1920, 1080);
+            this.MinimumSize = new System.Drawing.Size(900, 450);
+            this.Name = "LobbyFinder";
+            this.Text = "LobbyFinder";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lobbyList;
+        private System.Windows.Forms.Label lobbyNameLabel;
+        private System.Windows.Forms.Label playersInLobbyLabel;
+        private System.Windows.Forms.Button joinLobbyButton;
+        private System.Windows.Forms.Button createLobbyButton;
+        private System.Windows.Forms.Button exitLobbyFinderButton;
+    }
+}
